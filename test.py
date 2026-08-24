0bd1624aeaf835aed210ad095354e308c1ba3099
Elnaz <elnaz.mo.98@gmail.com>
2026-08-24 11:59:42 +0200

    test

---
+++ test.py
@@ -0,0 +1,9 @@
+import click
+
+@click.command()
+@click.option('--device', required=True, help='Target device')
+def main(device):
+    print(f"Hello World, the device is {device}!")
+
+if __name__ == 'main':
+    main()
